2b287bc284837b64408aa9b825789c50ae4cf8f8
timofey <maslovtimofey@gmail.com>
2023-11-26 20:53:14 +0500

    implement BackstagePass

--- items/backstage_pass.rb
+++ items/backstage_pass.rb
@@ -3,5 +3,21 @@
 require_relative 'item'
 
 class BackstagePass < Item
-  def update_quality; end
+  MAX_QUALITY = 50
+
+  def update_quality
+    @quality += quality_change
+    @quality = [@quality, MAX_QUALITY].min
+    @sell_in -= 1
+  end
+
+  private
+
+  def quality_change
+    return -@quality if @sell_in.zero?
+    return 3 if @sell_in < 6
+    return 2 if @sell_in < 11
+
+    1
+  end
 end
