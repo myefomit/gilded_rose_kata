2822f9c31678f3af311a73d3cd3f905278842ca1
timofey <maslovtimofey@gmail.com>
2023-11-26 20:34:34 +0500

    implement ConjuredItem

--- items/conjured_item.rb
+++ items/conjured_item.rb
@@ -3,5 +3,11 @@
 require_relative 'item'
 
 class ConjuredItem < Item
-  def update_quality; end
+  MIN_QUALITY = 0
+
+  def update_quality
+    @quality -= @sell_in.positive? ? 2 : 4
+    @quality = [@quality, MIN_QUALITY].max
+    @sell_in -= 1
+  end
 end
