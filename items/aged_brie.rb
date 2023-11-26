ae01a123182c1423a0acb0a3f98e858d6307f9f1
timofey <maslovtimofey@gmail.com>
2023-11-26 20:36:31 +0500

    implement AgedBrie

--- items/aged_brie.rb
+++ items/aged_brie.rb
@@ -3,5 +3,11 @@
 require_relative 'item'
 
 class AgedBrie < Item
-  def update_quality; end
+  MAX_QUALITY = 50
+
+  def update_quality
+    @quality -= @sell_in.positive? ? -1 : -2
+    @quality = [@quality, MAX_QUALITY].min
+    @sell_in -= 1
+  end
 end
