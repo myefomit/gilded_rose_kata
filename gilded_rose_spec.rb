f235776853d208f490f7edc527f1ca68549ab1ae
timofey <maslovtimofey@gmail.com>
2023-11-26 22:46:56 +0500

    refactor BackstagePass spec

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -3,40 +3,6 @@
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 Dir[File.join(File.dirname(__FILE__), 'items/*.rb')].each { |file| require file }
 
-shared_examples 'an item' do
-  it 'does not change the name' do
-    expect(item.name).to eq name
-  end
-
-  it 'changes sell_in' do
-    expect(item.sell_in).to eq(sell_in + sell_in_change)
-  end
-
-  it 'changes quality' do
-    expect(item.quality).to eq(quality + expected_quality_change)
-  end
-end
-
-shared_examples 'an item with decreasing quality' do
-  context 'when quality + expected_quality_change < min_quality' do
-    let(:quality) { min_quality + expected_quality_change.abs - 1 }
-
-    it 'sets item quality to min_quality' do
-      expect(item.quality).to eq(min_quality)
-    end
-  end
-end
-
-shared_examples 'an item with increasing quality' do
-  context 'when quality + expected_quality_change > max_quality' do
-    let(:quality) { max_quality - expected_quality_change + 1 }
-
-    it 'sets item quality to max_quality' do
-      expect(item.quality).to eq(max_quality)
-    end
-  end
-end
-
 describe GildedRose do
   describe '#update_quality' do
     let(:sell_in) { 10 }
@@ -47,44 +13,5 @@ describe GildedRose do
     let(:item) { item_class.new(name, sell_in, quality) }
     let(:items) { [item] }
     subject! { described_class.new(items).update_quality }
-
-    describe 'Backstage passes to a TAFKAL80ETC concert' do
-      let(:item_class) { BackstagePass }
-      let(:name) { 'Backstage passes to a TAFKAL80ETC concert' }
-      let(:quality) { 0 }
-
-      context 'when more than 10 days until concert' do
-        let(:sell_in) { 11 }
-        let(:expected_quality_change) { 1 }
-
-        it_behaves_like 'an item'
-        it_behaves_like 'an item with increasing quality'
-      end
-
-      context 'when 10 days or less' do
-        let(:sell_in) { 10 }
-        let(:expected_quality_change) { 2 }
-
-        it_behaves_like 'an item'
-        it_behaves_like 'an item with increasing quality'
-      end
-
-      context 'when 5 days or less' do
-        let(:sell_in) { 5 }
-        let(:expected_quality_change) { 3 }
-
-        it_behaves_like 'an item'
-        it_behaves_like 'an item with increasing quality'
-      end
-
-      context 'when after concert' do
-        let(:sell_in) { 0 }
-        let(:quality) { 42 }
-        let(:expected_quality_change) { -42 }
-
-        it_behaves_like 'an item'
-        it_behaves_like 'an item with decreasing quality'
-      end
-    end
   end
 end
---
+++ spec/items/backstage_pass_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require_relative '../../items/backstage_pass'
+require_relative 'shared_examples'
+require_relative 'shared_context'
+
+describe BackstagePass do
+  describe '#update_quality' do
+    include_context 'item context'
+
+    let(:name) { 'Backstage passes to a TAFKAL80ETC concert' }
+    let(:quality) { 0 }
+
+    context 'when more than 10 days until concert' do
+      let(:sell_in) { 11 }
+      let(:expected_quality_change) { 1 }
+
+      it_behaves_like 'an item'
+      it_behaves_like 'an item with increasing quality'
+    end
+
+    context 'when 10 days or less' do
+      let(:sell_in) { 10 }
+      let(:expected_quality_change) { 2 }
+
+      it_behaves_like 'an item'
+      it_behaves_like 'an item with increasing quality'
+    end
+
+    context 'when 5 days or less' do
+      let(:sell_in) { 5 }
+      let(:expected_quality_change) { 3 }
+
+      it_behaves_like 'an item'
+      it_behaves_like 'an item with increasing quality'
+    end
+
+    context 'when after concert' do
+      let(:sell_in) { 0 }
+      let(:quality) { 42 }
+      let(:expected_quality_change) { -42 }
+
+      it_behaves_like 'an item'
+      it_behaves_like 'an item with decreasing quality'
+    end
+  end
+end
