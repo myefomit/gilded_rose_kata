276938fcc203f7ad2079e8210d0a10101b57a6f1
timofey <maslovtimofey@gmail.com>
2023-11-26 18:07:15 +0500

    add spec for items with variable quality corner cases

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -16,11 +16,33 @@ shared_examples 'an item' do
   end
 end
 
+shared_examples 'an item with decreasing quality' do
+  context 'when quality + quality_change < min_quality' do
+    let(:quality) { min_quality + quality_change.abs - 1 }
+
+    it 'sets item quality to min_quality' do
+      expect(item.quality).to eq(min_quality)
+    end
+  end
+end
+
+shared_examples 'an item with increasing quality' do
+  context 'when quality + quality_change > max_quality' do
+    let(:quality) { max_quality - quality_change + 1 }
+
+    it 'sets item quality to max_quality' do
+      expect(item.quality).to eq(max_quality)
+    end
+  end
+end
+
 describe GildedRose do
   describe '#update_quality' do
     let(:sell_in) { 10 }
     let(:quality) { 20 }
     let(:sell_in_change) { -1 }
+    let(:min_quality) { 0 }
+    let(:max_quality) { 50 }
     let(:item) { Item.new(name, sell_in, quality) }
     let(:items) { [item] }
     subject! { described_class.new(items).update_quality }
@@ -28,59 +50,49 @@ describe GildedRose do
     describe 'basic item' do
       let(:name) { 'basic item' }
       let(:quality_change) { -1 }
-      let(:min_quality) { 0 }
 
       it_behaves_like 'an item'
+      it_behaves_like 'an item with decreasing quality'
 
       context 'when sell by date has passed' do
         let(:sell_in) { 0 }
+        let(:quality_change) { -2 }
 
         it 'decreases quality twice as fast' do
-          expect(item.quality).to eq(quality + quality_change * 2)
+          expect(item.quality).to eq(quality + quality_change)
         end
-      end
 
-      context 'when min quality' do
-        let(:quality) { min_quality }
-
-        it 'does not decrease quality' do
-          expect(item.quality).to eq(min_quality)
-        end
+        it_behaves_like 'an item with decreasing quality'
       end
     end
 
     describe 'Aged Brie' do
       let(:name) { 'Aged Brie' }
       let(:quality_change) { 1 }
-      let(:max_quality) { 50 }
 
       it_behaves_like 'an item'
-
-      context 'when max quality' do
-        let(:quality) { max_quality }
-
-        it 'does not increase quality' do
-          expect(item.quality).to eq(max_quality)
-        end
-      end
+      it_behaves_like 'an item with increasing quality'
     end
 
     describe 'Sulfuras, Hand of Ragnaros' do
       let(:name) { 'Sulfuras, Hand of Ragnaros' }
       let(:sell_in_change) { 0 }
       let(:quality_change) { 0 }
+      let(:quality) { 80 }
 
       it_behaves_like 'an item'
     end
 
     describe 'Backstage passes to a TAFKAL80ETC concert' do
       let(:name) { 'Backstage passes to a TAFKAL80ETC concert' }
+      let(:quality) { 0 }
 
       context 'when more than 10 days until concert' do
         let(:sell_in) { 11 }
         let(:quality_change) { 1 }
 
         it_behaves_like 'an item'
+        it_behaves_like 'an item with increasing quality'
       end
 
       context 'when 10 days or less' do
@@ -88,6 +100,7 @@ describe GildedRose do
         let(:quality_change) { 2 }
 
         it_behaves_like 'an item'
+        it_behaves_like 'an item with increasing quality'
       end
 
       context 'when 5 days or less' do
@@ -95,14 +108,16 @@ describe GildedRose do
         let(:quality_change) { 3 }
 
         it_behaves_like 'an item'
+        it_behaves_like 'an item with increasing quality'
       end
 
       context 'when after concert' do
         let(:sell_in) { 0 }
-        let(:quality) { 0 }
-        let(:quality_change) { 0 }
+        let(:quality) { 42 }
+        let(:quality_change) { -42 }
 
         it_behaves_like 'an item'
+        it_behaves_like 'an item with decreasing quality'
       end
     end
   end
