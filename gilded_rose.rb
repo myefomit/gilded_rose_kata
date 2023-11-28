dfa22739f513b0d1c35c0847e0e33d6fa0264462
timofey <maslovtimofey@gmail.com>
2023-11-28 16:50:15 +0500

    add basic item updater

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
+Dir[File.join(File.dirname(__FILE__), 'items/*.rb')].each { |file| require file }
+
 class GildedRose
   def initialize(items)
     @items = items
   end
 
   def update_quality
-    @items.each(&:update_quality)
+    @items.each do |item|
+      BasicItem.new(item).update_quality
+    end
   end
 end
